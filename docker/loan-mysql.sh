b8a517bcc9bf382fcb4ccaa51c9b8213c0359f2d
엄수혁 <soohyuk96@naver.com>
2023-08-02 22:18:14 +0900

    Revert "[feat] 개발 환경 점검 및 유기적인 작동 확인 (#5)" (#6)
    This reverts commit df8dc60940c1eeb802d463c15f72e900b13eece1.

--- docker/loan-mysql.sh
+++
@@ -1,9 +0,0 @@
-docker run -d \
-  --name loan-mysql \
-  -e MYSQL_ROOT_PASSWORD=root \
-  -e MYSQL_USER=soohyuk \
-  -e MYSQL_PASSWORD=yuki0326.. \
-  -e MYSQL_DATABASE=loan \
-  -e TZ=Asia/Seoul \
-  -p 3306:3306 \
-  mysql:latest
