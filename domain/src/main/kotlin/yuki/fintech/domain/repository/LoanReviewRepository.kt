49b08e237813e2085443cf290cfab35229a12ef5
endlessmomo <soohyuk96@naver.com>
2023-08-02 22:08:21 +0900

    feat : repository 메소드 정의

--- domain/src/main/kotlin/yuki/fintech/domain/repository/LoanReviewRepository.kt
+++ domain/src/main/kotlin/yuki/fintech/domain/repository/LoanReviewRepository.kt
@@ -3,4 +3,6 @@ package yuki.fintech.domain.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import yuki.fintech.domain.domain.LoanReview
 
-interface LoanReviewRepository : JpaRepository<LoanReview, Long>
+interface LoanReviewRepository : JpaRepository<LoanReview, Long> {
+    fun findByUserKey(userKey: String): LoanReview?
+}
--- domain/src/main/kotlin/yuki/fintech/domain/repository/UserInfoRepository.kt
+++ domain/src/main/kotlin/yuki/fintech/domain/repository/UserInfoRepository.kt
@@ -3,4 +3,6 @@ package yuki.fintech.domain.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import yuki.fintech.domain.domain.UserInfo
 
-interface UserInfoRepository : JpaRepository<UserInfo, Long>
+interface UserInfoRepository : JpaRepository<UserInfo, Long>{
+    fun findByUserKey(userKey:String):UserInfo
+}
