9f233e7f726dd68a84e9180ed3b0c334ce80bf44
endlessmomo <soohyuk96@naver.com>
2023-08-02 22:08:01 +0900

    build(api, domain, total) : 의존성 오류로 인한 설정

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,8 +5,6 @@ version = "0.0.1"
 dependencies {
     // spring-boot-starter-web
     implementation("org.springframework.boot:spring-boot-starter-web")
-
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
-
     implementation(project(":domain"))
 }
