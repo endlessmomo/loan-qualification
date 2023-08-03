437611521cf5505dd9798e1b03f38aa846ed47ef
엄수혁 <soohyuk96@naver.com>
2023-08-03 11:44:29 +0900

    [feat] 대출 심사 및 심사 요청 유저 정보 저장 기능 구현 (#9)
    대출 심사 및 유저 관련 정보 repository, service, controller 구현

---
+++ api/src/main/kotlin/yuki/fintech/api/loan/request/LoanRequestController.kt
@@ -0,0 +1,23 @@
+package yuki.fintech.api.loan.request
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/loan/api/v1")
+class LoanRequestController(
+    private val loanRequestService: LoanRequestServiceImpl
+) {
+
+    @PostMapping("/request")
+    fun loanRequest(
+        @RequestBody loanRequestInputDto: LoanRequestDto.LoanRequestInputDto
+    ): ResponseEntity<LoanRequestDto.LoanRequestResponseDto> {
+        return ResponseEntity.ok(
+            loanRequestService.loanRequestMain(loanRequestInputDto)
+        )
+    }
+}
